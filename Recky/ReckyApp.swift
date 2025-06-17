b7eac6f2bc57de5e601ab55a59644dc836505539
PJ Winters <winterspj@gmail.com>
2025-06-17 19:34:56 -0400

    Initial Commit

---
+++ Recky/ReckyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReckyApp.swift
+//  Recky
+//
+//  Created by Paul Winters on 6/17/25.
+//
+
+import SwiftUI
+
+@main
+struct ReckyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ReckyTests/ReckyTests.swift
@@ -0,0 +1,17 @@
+//
+//  ReckyTests.swift
+//  ReckyTests
+//
+//  Created by Paul Winters on 6/17/25.
+//
+
+import Testing
+@testable import Recky
+
+struct ReckyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
